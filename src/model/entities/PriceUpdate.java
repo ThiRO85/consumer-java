7182601f0bd44405b59880c083dd5651e964086b
Thiago Rodrigues <thgr85@gmail.com>
2021-01-31 11:24:08 -0200

    Implementacao da interface

---
+++ src/model/entities/PriceUpdate.java
@@ -0,0 +1,11 @@
+package model.entities;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.10);
+    }
+}
