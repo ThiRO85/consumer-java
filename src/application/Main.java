dd3f7e32cf4a23519c8d6e031f252f073dcdefa8
Thiago Rodrigues <thgr85@gmail.com>
2021-01-31 11:52:34 -0200

    Expressao lambda declarada

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -18,7 +19,9 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.10);
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
     }
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -31,7 +31,7 @@ public class Product {
     }
 
     public void nonStaticPriceUpdate() {
-        setPrice(getPrice() * 1.10);
+        price *= 1.10;
     }
 
     @Override
